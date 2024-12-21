8cd53a11c19d311c078a49d6eb443b2d20c5e683
HienKaito <165780424+HienKaito@users.noreply.github.com>
2024-12-21 09:36:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
-const port = 3000;
+const port = 10000;
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on ${port}`);
 });
